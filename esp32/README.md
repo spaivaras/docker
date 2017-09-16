984459eab91f69ed6d74a047c115a899fde9340a
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2017-09-16 18:28:12 +0300

    Initial image

---
+++ esp32/README.md
@@ -0,0 +1,2 @@
+Description will follow
+
---
+++ esp32/build/setup_docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# Install dependencies
+apt-get update
+apt-get install -y --no-install-recommends build-essential libncurses-dev flex bison gperf python python-serial
+
+# Prepare file locations
+mkdir -p /opt/esp
+cd /opt/esp
+
+# Download xtensa build tools (Change with new version)
+wget -O xtensa.tar.gz https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
+tar -xzf xtensa.tar.gz
+rm xtensa.tar.gz
+
+# Download esp-idf
+git clone --recursive https://github.com/espressif/esp-idf.git
+
+# Setup file owners
+chown -R project.project /opt/esp
+
+# Copy runtime files
+cp -frv /build/files/* / | true
+
+# Setup environment
+echo 'export PATH="$PATH:/opt/esp/xtensa-esp32-elf/bin"' >> /home/project/.bashrc
+echo 'export IDF_PATH="/opt/esp/esp-idf"' >> /home/project/.bashrc
+
+# Clean up APT when done.
+source /usr/local/build_scripts/cleanup_apt.sh
+
